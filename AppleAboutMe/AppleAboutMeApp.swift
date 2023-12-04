669ec322117a0d709794a75e2239de5ca43c1ea2
Ryan Matsuo <rymatsuo@gmail.com>
2023-12-04 12:22:27 -0800

    Initial Commit

---
+++ AppleAboutMe/AppleAboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleAboutMeApp.swift
+//  AppleAboutMe
+//
+//  Created by Ryan Matsuo on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleAboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
